f3adbf3efce41fd94d2ee233df8c7105bfecdd63
Erdinch <erdinchorhan02@gmail.com>
2022-07-09 22:13:55 +0300

    made a "stable" working version

--- js/script.js
+++ js/script.js
@@ -1,15 +1,44 @@
-let options = ['Rock', 'Paper', 'Scissors'];
-
 function computerPlay() {
+  let options = ['rock', 'paper', 'scissors'];
   let option = options[Math.floor(Math.random() * options.length)];
+  return option;
 }
 
-let player = prompt('Rock, Paper or Scissors?').toLowerCase();
+let computerSelection = computerPlay();
+console.log('Computer selected : ' + computerSelection);
 
-function playerSelection(player) {
-  if (player != 'rock' && player != 'paper' && player != 'scissors') {
+function playerPlay() {
+  let player = prompt('Rock, Paper or Scissors?').toLowerCase();
+  if (
+    player !== 'rock' &&
+    player !== 'paper' &&
+    player !== 'scissors'
+  ) {
     alert('Not a valid choice.');
+  } else {
+    return player;
   }
 }
 
-playerSelection(player);
+let playerSelection = playerPlay();
+console.log('You selected : ' + playerSelection);
+
+function playRound(playerSelection, computerSelection) {
+  if (
+    (computerSelection === `rock` && playerSelection === `scissors`) ||
+    (computerSelection === `paper` && playerSelection === `rock`) ||
+    (computerSelection === `scissors` && playerSelection === `paper`)
+  ) {
+    alert(`Computer selected ${computerSelection}. You lost.`);
+  } else if (
+    (playerSelection === `paper` && computerSelection === `rock`) ||
+    (playerSelection === `scissors` && computerSelection === `paper`) ||
+    (playerSelection === `rock` && computerSelection === `scissors`)
+  ) {
+    alert(`Computer selected ${computerSelection}. You win!`);
+  } else {
+    alert("It's a tie");
+  }
+}
+
+playRound(playerSelection, computerSelection);
